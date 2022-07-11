3cc38f53b2035f180f97cde7e8a10163f38ba110
Anna Schuppe <annaschuppe@eduroam-10-200-23-111.local.su.se>
2022-07-11 14:10:31 +0200

    first plot

--- analysis/analysis.py
+++ analysis/analysis.py
@@ -1,12 +1,25 @@
+#%% codecell
 import pandas as pd
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import plot, show
 
 from settings import MEASUREMENTS, TAXONOMY
 
+
+#%% codecell
 measurementset = MEASUREMENTS / 'bacteriahalocyanin.csv'
 
 taxonomyset = TAXONOMY / 'halocyanin_bacteria.csv'
 
 properties = pd.read_csv(measurementset)
 taxi = pd.read_csv(taxonomyset)
+pt = properties.merge(taxi, on='id')
 
-print(properties.merge(taxi, on='id', ))
+
+#%%codecell
+
+plt.figure()
+plt.plot(pt.dpm, pt.hpm)
+
+# %%
