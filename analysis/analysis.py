424039bcd2959d8741400911c1e0183c1b15cb67
Anna Schuppe <annaschuppe@eduroam-10-200-40-215.local.su.se>
2022-07-18 16:57:04 +0200

    plot finished, shortening of # of phylum not

--- analysis/analysis.py
+++ analysis/analysis.py
@@ -1,25 +1,35 @@
-#%% codecell
 import pandas as pd
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 from matplotlib.pyplot import plot, show
+import seaborn as sns
 
-from settings import MEASUREMENTS, TAXONOMY
+from settings import MEASUREMENTS, TAXONOMY, ANALYSIS
 
-
-#%% codecell
 measurementset = MEASUREMENTS / 'bacteriahalocyanin.csv'
-
 taxonomyset = TAXONOMY / 'halocyanin_bacteria.csv'
+analysisfolder = ANALYSIS/'bacteriahalocyanin'
 
+# Read csv files and merge
 properties = pd.read_csv(measurementset)
 taxi = pd.read_csv(taxonomyset)
 pt = properties.merge(taxi, on='id')
 
+# slice pt for only toptenphyla 
+toptenphyla = pt.value_counts(subset='phylum').index[:10]
+# drop rows if not in list
+#print(pt.phylum.values)
+rowstoremove = []
+#for i in pt.iterrows():
+for i in pt.iterrows():
+    if pt.phylum.values[i] in toptenphyla:
+        print('yes')
+    #else:
+    #    rowstoremove.append()
 
-#%%codecell
-
-plt.figure()
-plt.plot(pt.dpm, pt.hpm)
-
-# %%
+#for x in pt.columns[1:12]:
+#    xname = str(x)
+#    for y in pt.columns[1:12]:
+#        yname = str(y)
+#        sns.relplot(x= f"{xname}", y= f"{yname}", hue ='phylum', data = pt)
+#        plt.savefig(analysisfolder/f"{xname}{yname}.png",bbox_inches='tight',dpi=200)
