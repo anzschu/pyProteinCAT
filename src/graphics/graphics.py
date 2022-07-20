5e496b0c985b7784d32c0a93fc487f2df0675626
Anna Schuppe <annaschuppe@eduroam-10-200-42-121.local.su.se>
2022-07-20 15:16:29 +0200

    hydrophobic vector

--- src/graphics/graphics.py
+++ src/graphics/graphics.py
@@ -11,21 +11,29 @@ from src.metrics import Builder
 
 
 def drawDipole(session, proteinfile):
-    vectorfile = generateVectorFile(proteinfile)
+    dvectorfile = generateVectorFile(proteinfile)[0]
     run(session, f"open {proteinfile}")
-    run(session, f"open {vectorfile}")
+    run(session, f"open {dvectorfile}")
     # clean up
-    os.remove(vectorfile)
+    os.remove(dvectorfile)
 
+def drawHydrophobe(session, proteinfile):
+    hvectorfile = generateVectorFile(proteinfile)[1]
+    run(session, f"open {proteinfile}")
+    run(session, f"open {hvectorfile}")
+    os.remove(hvectorfile)
 
-def generateVectorFile(proteinfile):
+def generateVectorFile(proteinfile): #maybe pass argument here
     proteinfile = Path(proteinfile)
-    vectorfile = proteinfile.stem + ".bild"
+    dvectorfile = proteinfile.stem + f"{'dipole'}"+ ".bild"
+    hvectorfile = proteinfile.stem + f"{'hydrophobe'}" + ".bild"
     parser = PDBParser(QUIET=True, structure_builder=Builder())
     structure = parser.get_structure(proteinfile.stem, proteinfile)
+    structure.measure()
     cx, cy, cz = structure.center_of_mass()
     dx, dy, dz = structure.dipolevector()
-    with open(vectorfile, "w") as vector:
+    hx, hy, hz = structure.hydrophobicvector()
+    with open(dvectorfile, "w") as vector:
         body = f"""
                 .color 1 1 1
                 .sphere {cx} {cy} {cz} 2
@@ -33,7 +41,15 @@ def generateVectorFile(proteinfile):
                 .arrow {cx} {cy} {cz} {dx} {dy} {dz} 1 3 0.75
                 """
         vector.write(textwrap.dedent(body))
-    return vectorfile
-
+    with open(hvectorfile, "w") as vector:
+        body = f"""
+                .color 1 1 1
+                .sphere {cx} {cy} {cz} 2
+                .color 0 1 0
+                .arrow {cx} {cy} {cz} {hx} {hy} {hz} 1 3 0.75
+                """
+        vector.write(textwrap.dedent(body))
+    return dvectorfile, hvectorfile
 
 drawDipole(session, "data/data1/1ris.pdb")
+drawHydrophobe(session, "data/data1/1ris.pdb")
