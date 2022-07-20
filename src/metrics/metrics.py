7556a4afa0b6bf3af523a30a75174be2a6fc92c1
evallina <eloyvallina33@gmail.com>
2022-07-20 12:03:21 +0200

    Testing different docstrings in Sphinx

--- src/metrics/metrics.py
+++ src/metrics/metrics.py
@@ -8,7 +8,7 @@ from Bio.SeqUtils.ProtParam import ProteinAnalysis
 from Bio.PDB.Residue import Residue
 from Bio.PDB.Polypeptide import three_to_one, standard_aa_names
 
-GLYXGLY_ASA = { # from Miller, Janin et al (1987)
+GLYXGLY_ASA = {  # from Miller, Janin et al (1987)
     "A": 113,
     "R": 241,
     "N": 158,
@@ -16,7 +16,7 @@ GLYXGLY_ASA = { # from Miller, Janin et al (1987)
     "C": 140,
     "Q": 189,
     "E": 183,
-    "G":  85,
+    "G": 85,
     "H": 194,
     "I": 182,
     "L": 180,
@@ -31,41 +31,47 @@ GLYXGLY_ASA = { # from Miller, Janin et al (1987)
     "V": 160,
 }
 
-hydrophobicityscale = { # from Guy, H.R., Biophys J. 47:61-70(1985)
+hydrophobicityscale = {  # from Guy, H.R., Biophys J. 47:61-70(1985)
     'A': 0.100,
-    'R':  1.910,
-    'N':  0.480,
-    'D':  0.780,
+    'R': 1.910,
+    'N': 0.480,
+    'D': 0.780,
     'C': -1.420,
-    'Q':  0.950,
-    'E':  0.830,
-    'G':  0.330,
+    'Q': 0.950,
+    'E': 0.830,
+    'G': 0.330,
     'H': -0.500,
     'I': -1.130,
     'L': -1.180,
-    'K':  1.400,
+    'K': 1.400,
     'M': -1.590,
     'F': -2.120,
-    'P':  0.730,
-    'S':  0.520,
-    'T':  0.070,
+    'P': 0.730,
+    'S': 0.520,
+    'T': 0.070,
     'W': -0.510,
     'Y': -0.210,
     'V': -1.270,
 }
 
+
 class ModStructure(Structure):
+    """
+    Subclass of Bio.PDB.Structure containing new methods and attributes for the
+    estimation of protein physichochemical properties from their PDB
+    coordinates.
+    """
 
     def __init__(self, id):
-        Structure.__init__(self,id)
+        Structure.__init__(self, id)
 
     def measure(self):
         self.sequence = self.sequencer()
         self.length = self.getlength()
-        self.MWkDa = self.molecularweight()/1000
-        self.fPos= self.aminocount()[0]/self.length
-        self.fNeg = self.aminocount()[1]/self.length
-        self.fFatty = self.aminocount()[2]/self.length
+        self.MWkDa = self.molecularweight() / 1000
+        self.fPos = self.aminocount()[0] / self.length
+        self.fNeg = self.aminocount()[1] / self.length
+        self.fFatty = self.aminocount()[2] / self.length
         self.sasa = self.calculate_sasa()
         self.nc = self.netcharge()
         self.ncd = self.netchargedensity()
@@ -74,6 +80,10 @@ class ModStructure(Structure):
         self.hpm = self.hydrophobicmoment()
 
     def serializer(self):
+        """
+        Returns a dictionary representing the structure's physicochemical
+        properties.
+        """
         measurements = {
             'id': self.id,
             'length': self.length,
@@ -128,7 +138,7 @@ class ModStructure(Structure):
         """
         calculator = ShrakeRupley()
         for chain in self.get_chains():
-            calculator.compute(chain, level="R") # calculate on the monomers
+            calculator.compute(chain, level="R")  # calculate on the monomers
             for residue in chain.get_residues():
                 residue.monosasa = residue.sasa.copy()
 
@@ -145,7 +155,7 @@ class ModStructure(Structure):
         Return net charge density from net charge and total SASA of the
         residues. WRONG
         '''
-        netchargedensity = self.netcharge()/self.sasa
+        netchargedensity = self.netcharge() / self.sasa
         return netchargedensity
 
     def dipolevector(self):
@@ -153,10 +163,12 @@ class ModStructure(Structure):
         Return dipole moment calculated from the dipole moments of the positive and negative residues.
         Source for dipolemoment equation: Felder, Prilusky, Silman, Sussman Nucleic Acids Research 2007
         '''
-        dipolpos = np.zeros((1,3))
-        dipolneg = np.zeros((1,3))
-        dipolpos = sum(residue.center_of_mass() for residue in self.get_residues() if residue.resletter in 'KR')
-        dipolneg = sum(residue.center_of_mass() for residue in self.get_residues() if residue.resletter in 'DE')
+        dipolpos = np.zeros((1, 3))
+        dipolneg = np.zeros((1, 3))
+        dipolpos = sum(residue.center_of_mass()
+                       for residue in self.get_residues() if residue.resletter in 'KR')
+        dipolneg = sum(residue.center_of_mass()
+                       for residue in self.get_residues() if residue.resletter in 'DE')
         return dipolpos - dipolneg
 
     def dipolemoment(self):
@@ -164,12 +176,13 @@ class ModStructure(Structure):
 
     def truehydrophobicity(self):
         '''
-        Calculate total hydrophobicity for residues that are more than 25% exposed to the surface.
+        Calculate total hydrophobicity for residues that are more than 25%
+        exposed to the surface.
         '''
         truehydrophobicity = 0
         for residue in self.get_residues():
-            #if residue.get_resname() in GLYXGLY_ASA:
-            sasaratio =  residue.sasa / GLYXGLY_ASA[residue.resletter]
+            # if residue.get_resname() in GLYXGLY_ASA:
+            sasaratio = residue.sasa / GLYXGLY_ASA[residue.resletter]
             if sasaratio > 0.25:
                 truehydrophobicity += hydrophobicityscale[residue.resletter]
         return truehydrophobicity
@@ -181,8 +194,9 @@ class ModStructure(Structure):
         '''
         hydrophobicmoment = 0
         for residue in self.get_residues():
-            #if residue.get_resname() in GLYXGLY_ASA:
-            hydrophobicmoment += (hydrophobicityscale[residue.resletter]* residue.sasa* (  residue.center_of_mass()- self.center_of_mass()))
+            # if residue.get_resname() in GLYXGLY_ASA:
+            hydrophobicmoment += hydrophobicityscale[residue.resletter] * residue.sasa * (residue.center_of_mass() - self.center_of_mass())
+
         return np.linalg.norm(hydrophobicmoment)
 
     def __str__(self):
@@ -191,20 +205,32 @@ class ModStructure(Structure):
     def __repr__(self):
         return self.__str__()
 
+
 class ModRes(Residue):
     def __init__(self, id, resname, segid):
         Residue.__init__(self, id, resname, segid)
-        self.resletter = three_to_one(resname) if resname in standard_aa_names else ""
+        self.resletter = three_to_one(
+            resname) if resname in standard_aa_names else ""
 
     def is_buried(self, threshold=0.25):
         """
         Return boolean depending on ratio of residue SASA with respect to
-        literature values in GLYXGLY_ASA and a threshold (default 0.25).
+        literature values in GLYXGLY_ASA.
+
+        :param threshold: ratio (0, 1) of residue's SASA over reference
+            Gly-X-Gly polymer value.
+        :type threshold: float
+        :return: bool
+        :raise AttributeError: if the ModRes.monosara attribute is not set;
+            if ModStructure.calculate_sasa didn't run.
+
         """
         try:
             return self.monosasa / GLYXGLY_ASA[self.resletter] <= threshold
         except AttributeError:
-            raise AttributeError("Attrib. monosasa not set: did you call structure.calculate_sasa()")
+            raise AttributeError(
+                "Attrib. monosasa not set: call structure.calculate_sasa first"
+            )
 
     def __str__(self):
         return f"ModRes instance {self.resname}"
@@ -215,34 +241,47 @@ class ModRes(Residue):
 
 class Builder(StructureBuilder):
     """
-    Bespoke subclassing of Bio.PDB.StructureBuilder.StructureBuilder to
-    incorporate ModRes and ModStructure into the Bio.PDB hierarchy.
+    Subclassing of Bio.PDB.StructureBuilder.StructureBuilder to
+    incorporate metrics.ModRes and metrics.ModStructure into the
+    Bio.PDB hierarchy.
 
     Usage:
     Pass a Builder instance to the PDBParser as a structure_builder parameter.
 
     Example:
     >>> parser = PDBParser(QUIET=1, structure_builder=Builder())
-    >>> s = parser.get_structure("ab1", "5iy5_AB1.pdb")
+    >>> s = parser.get_structure("code", "1ris.pdb")
     """
+
     def init_structure(self, structure_id):
+        """
+        Instantiate structure object as a metrics.ModStructure object. Overrides
+        Bio.PDB.StructureBuilder.init_structure.
+        """
         self.structure = ModStructure(structure_id)         # ---> bespoke class
 
     def init_residue(self, resname, field, resseq, icode):
-        if not is_aa(resname, standard = True):
+        """
+        Instantiate residue as src.metrics.metrics.ModRes object. Overrides
+        Bio.PDB.StructureBuilder.init_residue.
+        """
+        if not is_aa(resname, standard=True):
             return
         if field != " ":
             if field == "H":
                 field = "H_" + resname
 
         residue_id = (field, resseq, icode)
-        self.residue = ModRes(residue_id, resname, self.segid)  # ---> bespoke class
+        self.residue = ModRes(
+            residue_id,
+            resname,
+            self.segid)  # ---> bespoke class
         self.chain.add(self.residue)
 
 
-if __name__ =='__main__':
+if __name__ == '__main__':
     parser = PDBParser(QUIET=1, structure_builder=Builder())
     s = parser.get_structure("1ris", "data/data1/1ris.pdb")
-    #s.calculate_sasa()
+    # s.calculate_sasa()
     s.measure()
     print(s.serializer())
