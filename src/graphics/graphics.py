e45c894bc7eb9d3a4b988d766e81339a27cca1f2
Anna Schuppe <annaschuppe@eduroam-10-200-29-81.local.su.se>
2022-07-21 14:03:27 +0200

    angles function in progress

--- src/graphics/graphics.py
+++ src/graphics/graphics.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import textwrap
+import argparse
 from pathlib import Path
 from Bio.PDB import PDBParser
 from chimerax.core.commands import run
@@ -9,6 +10,8 @@ sys.path.append(Path(__file__).parent.parent.parent.as_posix())
 
 from src.metrics import Builder
 
+def angles(session, proteinfile):
+    raise NotImplementedError
 
 def drawDipole(session, proteinfile):
     dvectorfile = generateVectorFile(proteinfile)[0]
@@ -23,7 +26,7 @@ def drawHydrophobe(session, proteinfile):
     run(session, f"open {hvectorfile}")
     os.remove(hvectorfile)
 
-def generateVectorFile(proteinfile): #maybe pass argument here
+def generateVectorFile(proteinfile):
     proteinfile = Path(proteinfile)
     dvectorfile = proteinfile.stem + f"{'dipole'}"+ ".bild"
     hvectorfile = proteinfile.stem + f"{'hydrophobe'}" + ".bild"
@@ -32,12 +35,12 @@ def generateVectorFile(proteinfile): #maybe pass argument here
     structure.measure()
     cx, cy, cz = structure.center_of_mass()
     dx, dy, dz = structure.dipolevector()
-    hx, hy, hz = structure.hydrophobicvector()
+    hx, hy, hz = structure.hydrophobicvector()/200
     with open(dvectorfile, "w") as vector:
         body = f"""
                 .color 1 1 1
                 .sphere {cx} {cy} {cz} 2
-                .color 1 0 0
+                .color 0 0 1
                 .arrow {cx} {cy} {cz} {dx} {dy} {dz} 1 3 0.75
                 """
         vector.write(textwrap.dedent(body))
@@ -45,11 +48,17 @@ def generateVectorFile(proteinfile): #maybe pass argument here
         body = f"""
                 .color 1 1 1
                 .sphere {cx} {cy} {cz} 2
-                .color 0 1 0
+                .color 1 {140/255} 0
                 .arrow {cx} {cy} {cz} {hx} {hy} {hz} 1 3 0.75
                 """
         vector.write(textwrap.dedent(body))
     return dvectorfile, hvectorfile
 
-drawDipole(session, "data/data1/1ris.pdb")
-drawHydrophobe(session, "data/data1/1ris.pdb")
+def main(fname):
+    drawDipole(session, fname)
+    drawHydrophobe(session, fname)
+
+parser = argparse.ArgumentParser()
+parser.add_argument("fname", help="pass filename to script")
+args = parser.parse_args()
+main(args.fname)
