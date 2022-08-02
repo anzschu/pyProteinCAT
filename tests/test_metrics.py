ea8a1e3fad4ca465641a2a1dcff2f9a053ab1099
Anna Schuppe <annaschuppe@eduroam-10-200-10-66.local.su.se>
2022-08-02 16:52:14 +0200

    added angle test

--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -16,3 +16,14 @@ class BuildingTests(unittest.TestCase):
             self.assertIsInstance(s, ModStructure)
             for residue in s.get_residues():
                 self.assertIsInstance(residue, ModRes)
+
+class AngleTests(unittest.TestCase):
+    def setUp(self):
+        self.u = (1,0,0)
+        self.v = [(1,1,0), (0,1,0), (-1,0,1)]
+        self.angle = [45, 90, 135]
+
+    def testAngle(self):
+        for v,an in zip(self.v, self.angle):                
+            a = ModStructure.anglemeasurement(self.u, v)
+            self.assertAlmostEqual(a, an)
